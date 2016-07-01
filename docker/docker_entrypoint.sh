6033c17db4290b2a3445bcf27b1f18babaf5d5bf
andras <andras@student.com>
2016-07-01 09:40:42 +0100

    Add welcome message (motd) + build info

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -7,7 +7,18 @@ if [[ "$(id -u)" -ne 0 ]]; then
     exit 1
 fi
 
+cat /etc/motd
+
+echo
+echo "Docker image release info:"
+cat /etc/devopshacks-release
+
+echo
 echo "Run confd with prefix ${CONFD_PREFIX}"
 confd -onetime -prefix ${CONFD_PREFIX} ${CONFD_OPTIONS}
+
+echo
+echo "The current user is ${USER}"
+echo
 
 exec su-exec ${USER} "$@"
